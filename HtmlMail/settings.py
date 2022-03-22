b2432d32b7510982d4a6d28db8d30b1c13136003
SushanthEnukonda <enukondasushanthreddy21@gmail.com>
2022-03-22 13:43:23 +0530

    env

--- HtmlMail/settings.py
+++ HtmlMail/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nxnp*wutfp=h+do%^7f$)k4^@yxwlk8(6er_xj*b5*8dl$m*(#'
+# SECRET_KEY = 'django-insecure-nxnp*wutfp=h+do%^7f$)k4^@yxwlk8(6er_xj*b5*8dl$m*(#'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -135,8 +136,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "sushanth.enukonda21@gmail.com"
-EMAIL_HOST_PASSWORD = "Sushanth@123"
+# EMAIL_HOST_USER = "sushanth.enukonda21@gmail.com"
+# EMAIL_HOST_PASSWORD = "Sushanth@123"
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 django_heroku.settings(locals())
