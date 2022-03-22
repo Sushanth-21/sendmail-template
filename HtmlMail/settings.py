e91cd2d14138fe78fce3ebda5e750fe0b1c1748a
SushanthEnukonda <enukondasushanthreddy21@gmail.com>
2022-03-22 13:23:50 +0530

    static

--- HtmlMail/settings.py
+++ HtmlMail/settings.py
@@ -125,6 +125,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static/"),
 )
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -134,7 +136,7 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "sushanth.enukonda21@gmail.com"
-EMAIL_HOST_PASSWORD = ""
+EMAIL_HOST_PASSWORD = "Sushanth@123"
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 django_heroku.settings(locals())
